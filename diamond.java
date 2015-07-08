f9463c16f2b7bf5a6c4bfe7165450538ba03af32
Varsha Palat <varshapalat@gmail.com>
2015-07-08 23:58:23 +0530

    5

--- diamond.java
+++ diamond.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
-class isotri
+class diamond
+
 {
 public static void main(String [] args)
 {
@@ -16,5 +17,18 @@ for(int k=0;k<i*2+1;k++)
 {
 System.out.print("*");
 }System.out.println();
+ }
+System.out.print(" ");
+for(int i=0;i<n-1;i++)
+{
+for(int l=0;l<i+1;l++)
+{System.out.print(" ");
 }
-}}
+for(int m=n-i*2;m>0;m--)
+{
+System.out.print("*");
+}
+
+System.out.println();
+
+}}}
