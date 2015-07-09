c5567747e3573266e67c5b231aa44cf69b03feb6
Varsha Palat <varshapalat@gmail.com>
2015-07-09 11:02:39 +0530

    6

--- diamond.java
+++ diamond.java
@@ -18,13 +18,14 @@ for(int k=0;k<i*2+1;k++)
 System.out.print("*");
 }System.out.println();
  }
-System.out.print(" ");
-for(int i=0;i<n-1;i++)
+//System.out.print(" ");
+for(int i=1;i<n;i++)
 {
-for(int l=0;l<i+1;l++)
+System.out.print(" ");
+for(int l=i;l>0;l--)
 {System.out.print(" ");
 }
-for(int m=n-i*2;m>0;m--)
+for(int m=(n-1)*2-1;m>(i-1)*2;m--)
 {
 System.out.print("*");
 }
